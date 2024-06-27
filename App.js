b87dd2e8d522c760e4594d71af1ef045912a16cd
salonishaw11 <121348227+salonishaw11@users.noreply.github.com>
2024-06-27 20:26:41 +0530

    chapter1

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "child1" }, "Hello world from React1!"),
+    React.createElement("h1", {}, "Hello world from React2!"),
+  ])
+);
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(heading);
