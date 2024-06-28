adcd6012cafcbab77c4a10f3c42e44ae21cf861c
salonishaw11 <121348227+salonishaw11@users.noreply.github.com>
2024-06-28 12:19:52 +0530

    chapter 3

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "child1" }, "Hello world from React1!"),
-    React.createElement("h1", {}, "Hello world from React2!"),
-  ])
-);
-const Root = ReactDOM.createRoot(document.getElementById("root"));
-Root.render(heading);
+const Heading2 = () => {
+  return <h2>heading2</h2>;
+};
+const Heading = () => {
+  return (
+    <div>
+      <h1>Hello from JSX</h1>
+      <Heading2 />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading />);
